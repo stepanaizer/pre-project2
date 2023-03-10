33188b1ee5512f4bf32dbd0ad8c3004c0018c0b9
StepanGusev <lazar.pwn@gmail.com>
2023-03-10 14:14:06 +0300

    Исправил инициализацию списка cars в CarServiceImpl, исправил метод getCars.

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -5,24 +5,26 @@ import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 @Service
 public class CarServiceImpl implements CarService {
+    private final List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("BMW", 520, "white"));
+        cars.add(new Car("Mercedes-Benz", 220, "red"));
+        cars.add(new Car("Nissan", 350, "silver"));
+        cars.add(new Car("Lexus", 300, "grey"));
+        cars.add(new Car("Honda", 777, "blue"));
+    }
+
     @Override
     public List<Car> getCars(Integer count) {
-
-        List<Car> cars = new ArrayList<>();
-
-        cars.add(new Car("BMW",520,"white"));
-        cars.add(new Car("Mercedes-Benz",220,"red"));
-        cars.add(new Car("Nissan",350,"silver"));
-        cars.add(new Car("Lexus",300,"grey"));
-        cars.add(new Car("Honda",777,"blue"));
-
-        if(count == null || count <= 0 || count >= 5){
+        if (count == null || count <= 0 || count >= 5) {
             return cars;
         } else {
-            return cars.stream().limit(count).collect(Collectors.toList());
+            return cars.stream().limit(count).toList();
         }
     }
 }
